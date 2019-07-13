b388aa2f416c2e7453fb321e16ef9f888124b778
koeida <koeida@github.com>
2019-07-13 14:45:25 -0400

    menu, focus

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from tkinter import *
 from tkinter.ttk import *
 from collections import namedtuple
 
+import tkinter as tk
+
 LaborSheetRow = namedtuple("LaborSheetRow", "area debit credit amount")
 
 names = ["Keegan", "Adder", "Megan", "Saoirse", "Stephan", "Brittany"]
 areas = ["DAIRY", "QUOTA", "GDN", "TOFU", "TOFU BOD"]
 areas.sort()
+
+MEMBER_WIDTH = 40
 
 cur_area_typing = ""
 cur_area_index = 0
@@ -74,35 +78,73 @@ def type_area(key,w):
 
     update_area_box(w, cur_area_typing, match)
 
+def area_get_focus(event):
+    #set cur_index to end of area name
+    #set cur_typing to entire text
+    pass
+
+def area_lose_focus(event):
+    global cur_area_index, cur_area_typing
+    event.widget.select_clear()
+    cur_area_index = 0
+    cur_area_typing = ""
+
+
 class LaborSheetForm():
     def __init__(self, master, num_rows=10):
         Label(master, text="Area").grid(row=0, column=0, sticky=W)
         Label(master, text="Debit").grid(row=0, column=1, sticky=W)
         Label(master, text="Credit").grid(row=0, column=2, sticky=W)
         Label(master, text="Amount").grid(row=0, column=3, sticky=W)
+        padding = 2
+        self.rows = []
         for i in range(num_rows):
-            self.area = Entry(master)
-            self.area.state(["readonly"])
-            self.area.bind('<KeyPress>', lambda k, x=self.area: type_area(k,x), "")
-            self.area.grid(row=i+1, column=0, sticky=W)
-            self.debit = Entry(master, width=8)
-            self.debit.grid(row=i+1, column=1, sticky=W)
-            self.credit = Entry(master, width=8)
-            self.credit.grid(row=i+1, column=2, sticky=W)
-            self.amount = Entry(master, width=8)
-            self.amount.grid(row=i+1, column=3, sticky=W)
+            area = Entry(master, state=DISABLED)
+            area.state(["readonly"])
+            area.bind('<KeyPress>', lambda k, x=area: type_area(k,x), "")
+            area.bind('<FocusOut>', area_lose_focus, "")
+            area.bind('<FocusIn>', area_get_focus, "")
+            area.grid(row=i+1, column=0, sticky=W, padx=padding, pady=padding)
+            debit = Entry(master, width=8, state=DISABLED)
+            debit.grid(row=i+1, column=1, sticky=W, padx=padding, pady=padding)
+            credit = Entry(master, width=8, state=DISABLED)
+            credit.grid(row=i+1, column=2, sticky=W, padx=padding, pady=padding)
+            amount = Entry(master, width=8, state=DISABLED)
+            amount.grid(row=i+1, column=3, sticky=W, padx=padding, pady=padding)
+            self.rows.append((area,debit,credit,amount))
+    def new_sheet(self, member):
+        for r in self.rows:
+            for element in r:
+                element["state"] = ACTIVE
+            r[0]["state"] = "readonly"
+        cur_member_label["width"] = 0
+        cur_member_label["text"] = member.center(MEMBER_WIDTH)
+
+        
+        
+
+
 
 root = Tk()
 root.option_add('*font',("sans", 24, ""))
 
+print(dir(tk))
+#foo = ComboBoxDialog
+
+mbar = Frame(root, relief=RAISED, borderwidth=2)
+mbar.pack(fill=X)
 top_frame = Frame(root)
 Button(top_frame, text="<-").pack(side=LEFT)
-Label(top_frame, text="Keegan").pack(side=LEFT)
+cur_member_label = Label(top_frame, text="".center(MEMBER_WIDTH), relief=SUNKEN, justify=CENTER)
+cur_member_label.pack(side=LEFT, padx=5, pady=10)
 Button(top_frame, text="->").pack(side=LEFT)
-
 
 sheet_frame = Frame(root)
 pwd = LaborSheetForm(sheet_frame)
+
+fileBtn = Button(mbar, text="New Sheet", underline=0)
+fileBtn.bind("<ButtonPress>", lambda x: pwd.new_sheet("Keegan"))
+fileBtn.pack(side=LEFT)
 
 top_frame.pack()
 sheet_frame.pack()
